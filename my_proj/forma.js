fe25d8ddbf26bb705f998b4ae9f3f34ab4de6f82
Eva <shaykina.eva@yandex.ru>
2023-12-26 09:26:56 +0300

    add email

--- my_proj/forma.js
+++ my_proj/forma.js
@@ -1,52 +1,54 @@
-function moveNavbar(){
-   
-}
+function moveNavbar() {}
 
-$(document).ready(function (){
-   
+$(document).ready(function () {
+  moveNavbar();
+  $(window).resize((event) => {
     moveNavbar();
-    $(window).resize((event)=>{
-        moveNavbar();
-    });
+  });
 
-    $("#fpolicy").change(function () {
-        if ($("#fpolicy").is(":checked")) {
-            $("#sendButton").prop("disabled", false);
-        } else {
-            $("#sendButton").prop("disabled", true);
-        }
-    });
-
-    let data = document.querySelectorAll(".info");
-    const ajaxSend = (formData) => {
-        fetch("https://formcarry.com/s/TSPqjN4-kTv", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(formData)
-        })
-            .then(function (response) {
-                alert("Ваша заявка принята! Ожидайте, в скором времени наши специалисты с Вами свяжутся!");
-                data.forEach((element) => { element.value = ""; });
-                $("#check").prop("checked", false);
-                $("#sendButton").prop("disabled", true);
-                localStorage.clear();
-            })
-            .catch((error) => {alert(error);})
-    };
-
-    const forms = $("#thatForm");
-    for (let i = 0; i < forms.length; i++) {
-        $("#sendButton").click(function (e) {
-            e.preventDefault();
-
-            let formData = new FormData(forms[i]);
-            formData = Object.fromEntries(formData);
-
-            ajaxSend(formData);
-        });
+  $("#fpolicy").change(function () {
+    if ($("#fpolicy").is(":checked")) {
+      $("#sendButton").prop("disabled", false);
+    } else {
+      $("#sendButton").prop("disabled", true);
     }
+  });
 
+  let data = document.querySelectorAll(".info");
+  const ajaxSend = (formData) => {
+    fetch("https://formcarry.com/s/x_qMQVZl4h", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(function (response) {
+        alert(
+          "Ваша заявка принята! Ожидайте, в скором времени наши специалисты с Вами свяжутся!"
+        );
+        data.forEach((element) => {
+          element.value = "";
+        });
+        $("#check").prop("checked", false);
+        $("#sendButton").prop("disabled", true);
+        localStorage.clear();
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  const forms = $("#thatForm");
+  for (let i = 0; i < forms.length; i++) {
+    $("#sendButton").click(function (e) {
+      e.preventDefault();
+
+      let formData = new FormData(forms[i]);
+      formData = Object.fromEntries(formData);
+
+      ajaxSend(formData);
+    });
+  }
 });
